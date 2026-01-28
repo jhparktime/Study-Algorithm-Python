8bfbd8b9e48750c238efe366670e7e4f4c18c87e
jhparktime <jhparktime@gmail.com>
2026-01-28 22:46:32 +0900

    Solve: 11508

--- Greedy/11508.py
+++ Greedy/11508.py
@@ -6,14 +6,11 @@ prices = []
 for _ in range(N):
     prices.append(int(sys.stdin.readline().rstrip()))
 
-# 가장 비싼 것부터 정렬
 prices.sort(reverse=True)
 
 answer = 0
 
 for i, price in enumerate(prices):
-    # 0,1,3,4,6,7,... 번째는 더하고
-    # 2,5,8,... 번째(매 3번째)는 공짜
     if i % 3 != 2:
         answer += price
 
